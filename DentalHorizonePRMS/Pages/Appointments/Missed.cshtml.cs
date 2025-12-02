640825ea54e553f2744d5f7452c7a4b5451adf37
libertineV <libertine.valdehueza16@gmail.com>
2025-12-02 11:21:11 +0800

    UI for login,home,services,addpatient,dashboard,

---
+++ DentalHorizonePRMS/Pages/Appointments/Missed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DentalHorizonePRMS.Pages.Appointments
+{
+    public class MissedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ DentalHorizonePRMS/Pages/Appointments/Upcoming.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DentalHorizonePRMS.Pages.Appointments
+{
+    public class UpcomingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
