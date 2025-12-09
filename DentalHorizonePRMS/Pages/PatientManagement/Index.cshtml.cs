893682196c96f7dd0b7eb1191ec19752eb38f286
meowwwge <angelicabalubayan5@gmail.com>
2025-12-10 03:36:00 +0800

    unnecessary and can be removed.

--- DentalHorizonePRMS/Pages/PatientManagement/Index.cshtml.cs
+++ DentalHorizonePRMS/Pages/PatientManagement/Index.cshtml.cs
@@ -1,12 +1,24 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DentalHorizonePRMS.Entities; // adjust namespace if needed
+using DentalHorizonePRMS.Interfaces;
 
 namespace DentalHorizonePRMS.Pages.PatientManagement
 {
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class IndexModel : PageModel
+	{
+		private readonly IPatientRepository _patientRepository;
+
+		public IndexModel(IPatientRepository patientRepository)
+		{
+			_patientRepository = patientRepository;
+		}
+
+		// Expose a collection of patients
+		public List<Patient> Patients { get; set; } = new();
+
+		public async Task OnGetAsync()
+		{
+			Patients = await _patientRepository.GetAllPatientsAsync();
+		}
+	}
 }
