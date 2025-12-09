a22b4be1ab2f59ddbb6ae6fdb458eacc000773bd
meowwwge <angelicabalubayan5@gmail.com>
2025-12-10 03:42:18 +0800

    added Required attributes

--- DentalHorizonePRMS/DTOs/Patients/PatientCreateDTO.cs
+++ DentalHorizonePRMS/DTOs/Patients/PatientCreateDTO.cs
@@ -1,19 +1,35 @@
-﻿namespace DentalHorizonePRMS.DTOs.Patients
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DentalHorizonePRMS.DTOs.Patients
 {
 	public class PatientCreateDTO
 	{
+		[Required]
 		public string PatientName { get; set; } = null!;
+		[Required]
 		public string Address { get; set; } = null!;
+		[Required]
 		public string Telephone { get; set; } = null!;
+		[Required]
 		public int Age { get; set; }
+		[Required]
 		public string Occupation { get; set; } = null!;
+		[Required]
 		public string Complaint { get; set; } = null!;
+		[Required]
 		public DateTime DateOfVisit { get; set; }
 		public DateTime? NextAppointment { get; set; }
+		[Required]
 		public string Service { get; set; } = null!;
+		
+		public string? Status { get; set; } = null!;
+		[Required]
 		public string VisitStatus { get; set; } = null!;
-		public string PatientStatus { get; set; } = null!; 
+		[Required]
+		public string PatientStatus { get; set; } = null!;
+		[Required]
 		public decimal Debit { get; set; }
+		[Required]
 		public decimal Credit { get; set; }
 	}
 }
