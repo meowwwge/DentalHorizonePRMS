101f7f9280fdb8c037d1121f4314a08f70f1b266
meowwwge <angelicabalubayan5@gmail.com>
2025-12-10 03:40:34 +0800

    deleted GetForManagementAsync, and GetForFinanceAsync. added CancelAppointmentsAsync, GetInactivePatients, ReschedulePatientAsync, SearchPatientsAsync

--- DentalHorizonePRMS/Interfaces/IPatientRepository.cs
+++ DentalHorizonePRMS/Interfaces/IPatientRepository.cs
@@ -14,10 +14,14 @@ namespace DentalHorizonePRMS.Interfaces
         Task<Patient?> GetByIdAsync(int id);
         Task<List<Patient>> GetAllPatientsAsync();
         Task<List<Patient>> GetAllActivePatientsAsync();
-        Task<IEnumerable<PatientManagementDTO>> GetForManagementAsync();
-        Task<IEnumerable<PatientFinanceDTO>> GetForFinanceAsync();
         Task<DashboardTotalsDTO> GetDashboardTotalsAsync();
         Task<IEnumerable<UpcomingAppointmentsDTO>> GetUpcomingAppointmentsAsync();
         Task<IEnumerable<MissedAppointmentsDTO>> GetMissedAppointmentsAsync();
-    }
+        Task<bool> CancelAppointmentAsync(int id);
+		Task<IEnumerable<Patient>> GetPatientsByDateAsync(int? month, int? year);
+        Task<IEnumerable<Patient>> GetInactivePatients();
+        Task<bool> ReschedulePatientAsync(int id, DateTime nextAppointment);
+        Task<IEnumerable<Patient>> SearchPatientsAsync(string keyword, string status);
+
+	}
 }
