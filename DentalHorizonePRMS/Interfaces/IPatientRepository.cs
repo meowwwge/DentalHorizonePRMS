3ad8410a611dae029d1bdea27f695e0756da226d
meowwwge <angelicabalubayan5@gmail.com>
2025-12-15 10:03:29 +0800

    removed SoftDelete, RestorePatient and GetInactivepatients. added GetArchivedPatients in IPatientRepository

--- DentalHorizonePRMS/Interfaces/IPatientRepository.cs
+++ DentalHorizonePRMS/Interfaces/IPatientRepository.cs
@@ -9,20 +9,17 @@ namespace DentalHorizonePRMS.Interfaces
     {
         Task<int> AddPatientAsync(Patient patient);
         Task<bool> UpdatePatientAsync(Patient patient);
-        Task<bool> SoftDeletePatientAsync(int id);
-        Task<bool> RestorePatientAsync(int id);
         Task<Patient?> GetByIdAsync(int id);
         Task<List<Patient>> GetAllPatientsAsync();
-        Task<List<Patient>> GetAllActivePatientsAsync();
+        Task<IEnumerable<Patient>> GetAllActivePatientsAsync();
         Task<DashboardTotalsDTO> GetDashboardTotalsAsync();
         Task<IEnumerable<UpcomingAppointmentsDTO>> GetUpcomingAppointmentsAsync();
         Task<IEnumerable<MissedAppointmentsDTO>> GetMissedAppointmentsAsync();
         Task<bool> CancelAppointmentAsync(int id);
 		Task<IEnumerable<Patient>> GetPatientsByDateAsync(int? month, int? year);
         Task<List<int>> GetAvailableYearsAsync();
-        Task<IEnumerable<Patient>> GetInactivePatients();
         Task<bool> ReschedulePatientAsync(int id, DateTime nextAppointment);
         Task<IEnumerable<Patient>> SearchPatientsAsync(string keyword, string status);
-
+        Task<IEnumerable<Patient>> GetArchivedPatientsAsync();
 	}
 }
